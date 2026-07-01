6bf4f2f33717dfbb2b12a4388231731fb59b83da
Jerome <jerome.guya@evsu.edu.ph>
2026-07-01 16:53:37 +0800

    Update token

--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -17,7 +17,6 @@ export const generateToken = (userId : number, res : Response) => {
     res.cookie("jwt", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
         maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
     });
     return token;
